8e196d423c095d44c9c49a30708e00f95a5cbc02
shalinik21 <92974847+shalinik21@users.noreply.github.com>
2024-07-05 03:39:01 +0000

    conversion complexities in js

--- js_basics/03_datatype_conv.js
+++ js_basics/03_datatype_conv.js
@@ -1,27 +1,68 @@
-let score = undefined
+// let score = undefined
 
-console.log(typeof(score))
+// console.log(typeof(score))
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
-//NaN : Not a number
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// //NaN : Not a number
 
-// '33' =>33
-//'33asd' =>NaN
-//true=>1;
+// // '33' =>33
+// //'33asd' =>NaN
+// //true=>1;
 
-let isLoggedIn="Shalu"
+// let isLoggedIn="Shalu"
 
-let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn)
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
 
-//1=>true;0=>false
-//""=>false
-//"shalu"=>true
+// //1=>true;0=>false
+// //""=>false
+// //"shalu"=>true
 
-let someNumber =33
+// let someNumber =33
 
-letStringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// letStringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*****************************Operations*****************
+
+let value =3
+let negValue = -value
+
+//console.log(negValue)
+
+// console.log(2**3)
+
+// let str1=" Shalini"
+// let str2="Hello"
+
+// let str3=str2+str1
+// console.log(str3)
+
+// //Complex situation 
+
+// console.log("1"+2);
+// console.log(1+"2");
+
+// console.log("1"+"2"+2)//122
+// console.log(1+2+"2")//32
+
+// console.log(+true)
+//console(+"");
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gameCounter =100;
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
