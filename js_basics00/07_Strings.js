2034bb7523407af930411c3de0969b5228489602
shalinik21 <92974847+shalinik21@users.noreply.github.com>
2024-07-05 09:36:41 +0000

    Strings in js

--- js_basics00/07_Strings.js
+++ js_basics00/07_Strings.js
@@ -3,7 +3,7 @@ const repoCount= 50
 
 // console.log(name + repoCount+" Value");
 
-console.log(`Hello my name is ${name} and my repo Count is${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo Count is${repoCount}`);
 
 const gameName = new String('shalusi-shalu-shili')
 
