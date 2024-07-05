87651742f34ca8ab2cebf03f4bf5c96dd2866317
shalinik21 <92974847+shalinik21@users.noreply.github.com>
2024-07-05 06:33:19 +0000

    Memory allocation in js

---
+++ js_basics/06_stack_Heap.js
@@ -0,0 +1,22 @@
+//Stack(primitive):Copy
+//Heap(Non-primitive):Reference
+
+let myname="Shalini"
+
+let anotherName=myname
+anotherName="Shashwat"
+
+console.log(myname);
+console.log(anotherName);
+
+let userOne={
+    email:"usera@google.com",
+    upi:"user@ybl"
+}
+
+let userTwo=userOne
+
+userTwo.email="shalu@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
