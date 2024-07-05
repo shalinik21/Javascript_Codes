f76bd9524661f3dde6b0e1a77ef2fa98da9810c6
shalinik21 <92974847+shalinik21@users.noreply.github.com>
2024-07-05 02:44:14 +0000

    variables in js

---
+++ js_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId= 234534
+var accountEmail ="shalini@yahoo.com"
+let password ="23415"
+accountCity="Jaipur"
+accountState=undefined;
+
+
+accountId=2345
+// TypeError: Assignment to constant variable
+console.table([accountId,accountEmail,password,accountCity,accountState])
+
+//var is not prefered because of issue of
+// block scope and functional scope
