a12625d87d8c0518c7f354f3a53f527476ca3871
shalinik21 <92974847+shalinik21@users.noreply.github.com>
2024-07-05 03:22:42 +0000

    Conversion in datatypes

---
+++ js_basics/03_datatype_conv.js
@@ -0,0 +1,27 @@
+let score = undefined
+
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//NaN : Not a number
+
+// '33' =>33
+//'33asd' =>NaN
+//true=>1;
+
+let isLoggedIn="Shalu"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+//1=>true;0=>false
+//""=>false
+//"shalu"=>true
+
+let someNumber =33
+
+letStringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
