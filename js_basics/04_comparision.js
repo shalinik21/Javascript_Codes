0160d2534322df670cbbe6b88fdff21259f84f9f
shalinik21 <92974847+shalinik21@users.noreply.github.com>
2024-07-05 03:46:24 +0000

    comparision in js

---
+++ js_basics/04_comparision.js
@@ -0,0 +1,17 @@
+// console.log(2>1)
+// console.log("2">1)
+//ensure same datatype
+
+// console.log(null>0);
+// console.log(null==0);//null is assigned 0
+// console.log(null>=0);//value conversion problem
+
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+//--Avoid the above comparision
+
+//=== =>strict check
+console.log("2"===2);//false
