189e045f0eaf295ef1bbd8a764cbc36a7f8ab501
shalinik21 <92974847+shalinik21@users.noreply.github.com>
2024-07-05 03:21:53 +0000

    datatypes in js

---
+++ js_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3) //we are using node js not browser
+
+//alert is not defined
+
+//Important --code readability should be high
+
+console.log("Shalini")
+
+let name ="shalu"
+let age =24
+let isLoggedIn = false
+
+
+//number => 2to pow 53
+//bigint
+//String
+//boolean =>true/false
+//null=>standalone value
+//undefined =>
+
+//symbol=>unique
+
+//object
+
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
